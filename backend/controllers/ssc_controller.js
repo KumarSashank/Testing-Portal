905ed08c41d1d3923eacf54e27f97d240519a378
KumarSashank <kumarsashank_ganta@srmap.edu.in>
2024-01-08 16:52:25 +0530

    create NOS route added

--- backend/controllers/ssc_controller.js
+++ backend/controllers/ssc_controller.js
@@ -31,3 +31,31 @@ module.exports.createQPS = async (req, res) => {
     res.send("Only Admin can create QPS");
   }
 };
+
+module.exports.createNOS = async (req, res) => {
+  console.log("create NOS");
+  const { SSC_code, adminId, QPS_name, NOS_name } = req.body;
+
+  if (adminId == "ZVbVLz0Jwqhmt13yivVHWhILdbN2") {
+    try {
+      // Find the document named with SSC_code
+      const sscDoc = await firestore.collection("SSC").doc(SSC_code).get();
+      const qpsDoc = await sscDoc.ref.collection("QPS").doc(QPS_name).get();
+
+      console.log(qpsDoc);
+
+      // Add a subcollection NOS
+      await qpsDoc.ref.collection("NOS").doc(NOS_name).set({
+        createdAt: new Date(),
+        NOS_name: NOS_name,
+      });
+
+      res.send("NOS created successfully!");
+    } catch (error) {
+      console.error("Error creating NOS:", error);
+      res.status(500).send("An error occurred while creating NOS.");
+    }
+  } else {
+    res.send("Only Admin can create NOS");
+  }
+};
--- backend/routes/ssc_route.js
+++ backend/routes/ssc_route.js
@@ -1,8 +1,10 @@
 const { Router } = require("express");
-const { createQPS } = require("../controllers/ssc_controller");
+const { createQPS, createNOS } = require("../controllers/ssc_controller");
 const router = Router();
 
 router.post("/create_qps", createQPS);
+router.post("/create_nos", createNOS);
+
 // router.post("/signup", signupUser); // Route for user signup
 // router.post("/signin/:role", signinUser); // Route for user signin with role parameter
 // router.post("/createSSC", createSSC); // Route for creating SSC
