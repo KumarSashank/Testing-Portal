6ce1f6b2c96ed382b4205574c579868cb83c154a
KumarSashank <kumarsashank_ganta@srmap.edu.in>
2024-04-03 21:55:16 +0530

    (Retrieving questions for students) API added

--- backend/controllers/student_controller.js
+++ backend/controllers/student_controller.js
@@ -20,6 +20,12 @@ module.exports.studentLogin = async (req, res) => {
       console.log("QPS:", qps); // Expected: UI
       console.log("Student Number:", studentNumber); // Expected: 101
 
+      const qpRef = await firestore.collection("Batches").doc(batchID).get();
+      if (!qpRef.exists) {
+        return res.status(404).json({ error: "Batch not found" });
+      }
+      const qpNum = qpRef.data().qpNo;
+
       // Fetch the batch document from Firestore
       const batchRef = await firestore
         .collection("Batches")
@@ -39,23 +45,65 @@ module.exports.studentLogin = async (req, res) => {
         return res.status(401).json({ error: "Invalid password" });
       }
 
-      // Here you would continue with your authentication logic
-      // For example, checking the password against what's stored in Firestore
-      // This is commented out as it depends on your specific Firestore structure
-
       // Assuming authentication is successful
-      return res
-        .status(200)
-        .json({
-          message: "Login successful",
-          batchID: batchID,
-          qps: qps,
-          studentNumber: studentNumber,
-        });
+      return res.status(200).json({
+        message: "Login successful",
+        batchID: batchID,
+        qps: qps,
+        studentNumber: studentNumber,
+        QPno: qpNum,
+      });
     } else {
       console.error("Invalid student ID format");
       return res.status(400).json({ error: "Invalid student ID format" });
     }
+  } catch (error) {
+    console.error("Error accessing Firestore:", error);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+//retrieving the questions from the database
+module.exports.getQuestionPaper = async (req, res) => {
+  const { qps, QPno } = req.body;
+  try {
+    const questionsRef = await firestore
+      .collection("question_papers")
+      .doc(qps)
+      .collection("papers")
+      .doc(QPno)
+      .collection("questions")
+      .get();
+
+    const questionsData = [];
+    questionsRef.forEach((doc) => {
+      const {
+        Option1,
+        Option2,
+        Option3,
+        Option4,
+        QuestionTypeID,
+        "s.no": sNo,
+        PCIDsWithMarks,
+        Question,
+        Marks,
+        QuestionLevelID,
+      } = doc.data();
+      questionsData.push({
+        Option1,
+        Option2,
+        Option3,
+        Option4,
+        QuestionTypeID,
+        "s.no": sNo,
+        PCIDsWithMarks,
+        Question,
+        Marks,
+        QuestionLevelID,
+      });
+    });
+
+    return res.status(200).json(questionsData);
   } catch (error) {
     console.error("Error accessing Firestore:", error);
     return res.status(500).json({ error: "Internal server error" });
--- backend/routes/student_route.js
+++ backend/routes/student_route.js
@@ -1,11 +1,13 @@
 const { Router } = require("express");
 const {
-    studentLogin,
+  studentLogin,
+  getQuestionPaper,
 } = require("../controllers/student_controller");
 const router = Router();
 
 // router.get("/tata", check_user);
 router.post("/studentLogin", studentLogin); // Route for user signup
+router.get("/getQuestions", getQuestionPaper);
 
 router.get("/hello", (req, res) => {
   res.send("Hello, World!");
