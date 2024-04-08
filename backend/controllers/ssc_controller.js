316ab07b48b9a28143addb852fc2002c8e567ae9
KumarSashank <kumarsashank_ganta@srmap.edu.in>
2024-04-08 15:31:59 +0530

    added QPS id and removed admin ID

--- backend/controllers/ssc_controller.js
+++ backend/controllers/ssc_controller.js
@@ -7,28 +7,25 @@ const firestore = db;
 
 module.exports.createQPS = async (req, res) => {
   console.log("create QPS");
-  const { SSC_code, adminId, QPS_name } = req.body;
+  const { SSC_code, QPS_name, QPS_id } = req.body;
 
-  if (adminId == "ZVbVLz0Jwqhmt13yivVHWhILdbN2") {
-    try {
-      // Find the document named with SSC_code
-      const sscDoc = await firestore.collection("SSC").doc(SSC_code).get();
+  try {
+    // Find the document named with SSC_code
+    const sscDoc = await firestore.collection("SSC").doc(SSC_code).get();
 
-      console.log(sscDoc);
+    console.log(sscDoc);
 
-      // Add a subcollection QPS
-      await sscDoc.ref.collection("QPS").doc(QPS_name).set({
-        createdAt: new Date(),
-        QPS_name: QPS_name,
-      });
+    // Add a subcollection QPS
+    await sscDoc.ref.collection("QPS").doc(QPS_name).set({
+      createdAt: new Date(),
+      QPS_name: QPS_name,
+      QPS_id: QPS_id,
+    });
 
-      res.send("QPS created successfully!");
-    } catch (error) {
-      console.error("Error creating QPS:", error);
-      res.status(500).send("An error occurred while creating QPS.");
-    }
-  } else {
-    res.send("Only Admin can create QPS");
+    res.send("QPS created successfully!");
+  } catch (error) {
+    console.error("Error creating QPS:", error);
+    res.status(500).send("An error occurred while creating QPS.");
   }
 };
 
