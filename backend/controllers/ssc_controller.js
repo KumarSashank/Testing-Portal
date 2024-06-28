a499940dc501302a51cf0331ba46023667cc0170
KumarSashank <kumarsashank_ganta@srmap.edu.in>
2024-06-28 13:35:21 +0530

    Create NOS architecture updated

--- backend/controllers/ssc_controller.js
+++ backend/controllers/ssc_controller.js
@@ -31,29 +31,26 @@ module.exports.createQPS = async (req, res) => {
 
 module.exports.createNOS = async (req, res) => {
   console.log("create NOS");
-  const { SSC_code, adminId, QPS_name, NOS_name } = req.body;
+  const { SSC_code, NOS_ID, NOS_name } = req.body;
 
-  if (adminId == "ZVbVLz0Jwqhmt13yivVHWhILdbN2") {
-    try {
-      // Find the document named with SSC_code
-      const sscDoc = await firestore.collection("SSC").doc(SSC_code).get();
-      const qpsDoc = await sscDoc.ref.collection("QPS").doc(QPS_name).get();
+  try {
+    // Find the document named with SSC_code
+    const sscDoc = await firestore.collection("SSC").doc(SSC_code).get();
+    // const qpsDoc = await sscDoc.ref.collection("QPS").doc(QPS_name).get();
 
-      console.log(qpsDoc);
+    console.log(sscDoc);
 
-      // Add a subcollection NOS
-      await qpsDoc.ref.collection("NOS").doc(NOS_name).set({
-        createdAt: new Date(),
-        NOS_name: NOS_name,
-      });
+    // Add a subcollection NOS
+    await sscDoc.ref.collection("NOS").doc(NOS_ID).set({
+      createdAt: new Date(),
+      NOS_name: NOS_name,
+      NOS_ID: NOS_ID,
+    });
 
-      res.send("NOS created successfully!");
-    } catch (error) {
-      console.error("Error creating NOS:", error);
-      res.status(500).send("An error occurred while creating NOS.");
-    }
-  } else {
-    res.send("Only Admin can create NOS");
+    res.send("NOS created successfully!");
+  } catch (error) {
+    console.error("Error creating NOS:", error);
+    res.status(500).send("An error occurred while creating NOS.");
   }
 };
 
